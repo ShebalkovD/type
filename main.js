a95e766f15c6b47166f44e201fdb0522bf860c36
DenisS <shebalkovdenis.wk@gmail.com>
2024-05-08 18:05:54 +0300

    Добавлено окно статистики, исправлен алгоритм обработки текста

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 let heading = document.querySelector('.text')  // тег, который отображает текст
-let stats = document.getElementById('stats')   // тег, который отображаем статистику
-let timer = document.getElementById('timer')   // тег, который отображаем статистику
+let stats = document.querySelector('.stats')   // тег, который отображает статистику
+let stats_speed = document.getElementById('speed') // тег, который отображает скорость
+let stats_mistakes = document.getElementById('mistakes')// тег, который отображает кол-во ошибок
+let stats_accuracy = document.getElementById('accuracy')// тег, который отображает точность
+let restart_btn = document.querySelector('.restart') // кнопка заново
+let timer = document.getElementById('timer')   // тег, который отображает время
 
-let text_counter = 0 // счетчик фраз в списке
+let isStart = true
+
 let text = [] // массив с символами для перебора и проверки
 let screen_text = []// массив для вывода на экран
 let children // массив символов, обернутых в span
 let i = 0 // счетчик символов 
-let number = text[i] // текущий символ
+let number  // текущий символ
 let text_shift = 0 // смещение текста
 let text_arr = ['--- здесь должен быть текст ---'] // фразы для вывода на экран
 
 let sign_counter = 0 // счетчик символов для статистики
+let mistakes = 0 // счетчик ошибок
 let time = 60 // время таймера
 
-// обрабатывает текст для печати и выводит на экран
-function fill_text(i) {
-    text = text_arr[i].split("")
+// обработка текста и вывод на экран
+function fill_text() {
+    text = text_arr[0].split("")
     screen_text = [] 
+    number = text[i]
 
     // наполнение массива символами, обернутыми в span
     for (let index = 0; index < text.length; index++) {
@@ -26,49 +33,50 @@ function fill_text(i) {
     }
 
     heading.innerHTML = screen_text.join('') // сборка строки и вывод на экран
+    heading.style.color = 'gray'
     children = heading.children // получение символов текущей строки
     children[0].classList.add('current') // добавление каретки первому символу
-    
-    reset()
 }
 
-// сброс прогресса текущей строки
-function reset(){
-
-    text_shift = 0
-    heading.style.transform = `translateX(${100 - text_shift}px)`
-
-    i = 0 // счетчик символов
-    number = text[i] // текущий символ
-
-    heading.style.color = 'gray'
-
-    for (let index = 0; index < children.length; index++) {
-        const element = children[index];
-        element.classList.remove('current') // сброс каретки
-        element.style.color = 'gray' // сброс цвета
-    }
-
-    children[0].classList.add('current') // добавление каретки для символа
+// разрешение запуска обработки нажатий, запуск таймера
+function start(){
+    isStart = true
+    start_timer()
 }
 
 // полный сброс прогресса (заново)
-function full_reset(){
-    sign_counter = 0
-    text_counter = 0
-    
-    fill_text(text_counter)
+function reset(){
     set_stats()
+    isStart = false
+    i = 0
+    sign_counter = 0
+    mistakes = 0
+    text_shift = 0
+
+    heading.style.transform = `translateX(100px)` // сдвиг сроки
+    heading.style.color = 'gray'
+    
+    fill_text()
     clearInterval(tick)
     clearTimeout(timer_id)
     // console.log('Таймер СТОП');
-    start_timer()
 }  
 
 // вывод статистики на экран
 function set_stats() {
     spm = Math.round(sign_counter)
-    stats.innerHTML = `Символов в минуту: ${spm}`
+
+    stats.style.display = 'flex'
+    stats_speed.innerHTML = `${spm} символов в минуту`
+    stats_mistakes.innerHTML = mistakes
+    accuracy = ((sign_counter*100)/(sign_counter+mistakes)).toFixed(2)
+    if (accuracy < 80) {
+        stats_accuracy.style.color = 'red'
+    }
+    else{
+        stats_accuracy.style.color = 'green'
+    }
+    stats_accuracy.innerHTML = `${accuracy}%`
 
     console.log('Кол-во символов:', sign_counter);
     console.log('Символов в минуту:', spm);
@@ -87,37 +95,32 @@ function start_timer() {
     timer_id = setTimeout(function() {
         // console.log('Таймер СТОП');
         clearInterval(tick)
-        set_stats()
+        reset()
     }, time * 1000)
 
     return tick
-}
+}   
 
 // основная функция для обработки нажатий клавиш
 function trackKeyboardActivity() {
     document.addEventListener('keydown', function(event) {
-        const keyPressed = event.key // получение нажатой клавиши
-        const keyCode = event.code // получение ее кода
+        if (isStart === true) {
+            const keyPressed = event.key // получение нажатой клавиши
+            const keyCode = event.code // получение ее кода
 
-        // Вывод в консоль целевого символа и нажатой клавиши, ее кода
-        // console.log('ЦЕЛЬ!' + number)  
-        // console.log('Key pressed: ' + keyPressed)
-        // console.log('Key code: ' + keyCode)
+            // Вывод в консоль целевого символа и нажатой клавиши, ее кода
+            console.log('ЦЕЛЬ!' + number)  
+            console.log('Key pressed: ' + keyPressed)
+            console.log('Key code: ' + keyCode)
 
-        // если нажата верная клавиша
-        if (number == keyPressed) {
-            sign_counter ++
+            // если нажата верная клавиша
+            if (number == keyPressed) {
+                sign_counter ++
 
-            // если символ последний в строке - переходим к новой
-            if (i == text.length-1 && text_counter != text_arr.length - 1) {
-                text_counter ++
-                fill_text(text_counter)
-            }
-            // если не последний
-            else {
                 children[i].style.color = 'white'
                 text_shift += children[i].offsetWidth
-                heading.style.transform = `translateX(${100 - text_shift}px)`
+                heading.style.transform = `translateX(${100 - text_shift}px)` // сдвиг сроки
+
                 // переход к следующему символу
                 i = i + 1 
                 number = text[i]
@@ -125,40 +128,40 @@ function trackKeyboardActivity() {
                 children[i-1].classList.remove('current') // удаление каретки для предыдущего символа
                 children[i].classList.add('current') // добавление каретки для следующего символа
 
-                
+                // Если символ последний выводим статистику
+                if (i == text.length-1) {
+                    set_stats()
+                }
             }
-
-            if (i == text.length-1 && text_counter == text_arr.length - 1) {
-                set_stats()
-            }
-        }
-        // если нажата неверная клавиша 
-        else {
-            // сброс тренажера при нажатии сочетания клавиш (Ctrl + Enter)
-            if (keyCode === 'Enter' && (event.ctrlKey || event.metaKey)){
-                full_reset()
-            }
+            // если нажата неверная клавиша 
             else {
-                // вывод ошибки на экран
-                children[i].style.color = 'red'
-                console.log('ERROR!' + number)
+                // сброс тренажера при нажатии сочетания клавиш (Ctrl + Enter)
+                if (keyCode === 'Enter' && (event.ctrlKey || event.metaKey)){
+                    reset()
+                }
+                else {
+                    mistakes++
+                    // вывод ошибки на экран
+                    children[i].style.color = 'red'
+                    console.log('ERROR!' + number)
+                }
             }
         }
+        
     })
 }
 
+restart_btn.addEventListener('click', function(){
+    stats.style.display = 'none'
+    start()
+})
+
 // Фразы для вывода на экран
 text_arr = [
-'первая строка первая строка первая строка первая строка',
-'первая строка первая строка первая строка первая строка',
-'первая строка первая строка первая строка первая строка',
-'первая строка первая строка первая строка первая строка',
-'первая строка первая строка первая строка первая строка',
-'вторая строка',
-'третья строка',
+    'пораженчество контекст вывевание ехидство трение кортик перепланировка общипывание темнолицая свинарка турбогенератор травинка оркестратор арба лесоруб конюшонок раскатчик фаталистичность воспламенение минимализм тензиометр провозгласительница отвисание свинооткорм продюсер смушка эфиронос тафта эргограф раздаривание сотерн пугало училище благоденствие безрассудство прищуривание монизм смысл расстилка односоставность монорельс откровенность скрипение мартинизм недоработка полировка метемпсихоз подкрепление проставление спидофобия гидрометрия елей рисовальщик приутюжка юродствование чернорясная скрадывание догма тархун буквализм вымачивание рокотание напружина близорукость горнорабочий базирование перегрунтовка статс-секретарь орлянка приспособленец сверстничество учан раздельность зоомагазин латания курильница дейтрон керосинка проформа установление колумбийка кроение черносмородиновая манна амнистированный перерасчет наперник пилигримство отклик кудесник вариант кримплен ассистент завышение разрешимость трезвенник накрахмаливание нагроможденность славист комсостав элефантиазис комплексность куток обрезок диспропорциональность узнавание виолончель побледнение краска прекос вылеживание агрохимик антабка задорина жизнелюбие консерватория антрепренер подстрагивание флокс лжетолкование вертикаль марселин славяновед грамматист иллириец исцелительница аббат приарендовывание патан гитлеризм познание непрочность мотогонщик тред-юнионизм переплетчица задорность блузка монокультура выпивание перетлевание сваебоец расфасовщик претор верность накрывальщик лисель несерьезность урбанистка пеленка футуролог дисгармоничность клоп директор наусник обвоз новогодие секунд-майор шванк астронавигация кривизна аргентина пироксилин металлолом обычай подрубка отходчивость бактериофаг бадминтонист аптекоуправление братан соболевание спайщица аэронавтика гидромонтажник камлот завизирование космоплавание горицвет пучение клетчатка дубняк аденовирус гуммигут эндемизм ездок субалтерн-офицер безнадежность яблоко ликование радиотелеграф просолка супница гомункул суржа наименование подшлемник перекантовка расстрелянная отпускник сахаризация'
 ] 
 
-fill_text(text_counter) // обработка текста и вывод на экран
+fill_text() // обработка текста и вывод на экран
 trackKeyboardActivity() // запуск отслеживания нажатия клавиш
 
 // таймер 
